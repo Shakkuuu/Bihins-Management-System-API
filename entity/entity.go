10bc9643aa3723620beea30dcc8503525fa26a10
Shakku <m21074@g.metro-cit.ac.jp>
2022-12-29 12:02:56 +0900

    登録日時(CreatedAt)の追加

--- entity/entity.go
+++ entity/entity.go
@@ -1,9 +1,12 @@
 package entity
 
+import "time"
+
 // User is user models property
 type Bihin struct {
-	ID          int    `json:"id"`
-	Shishutsuid string `json:"shishutsuid"`
-	Dantaimei   string `json:"dantaimei"`
-	Bihin       string `json:"bihin"`
+	ID          int       `json:"id"`
+	Shishutsuid string    `json:"shishutsuid"`
+	Dantaimei   string    `json:"dantaimei"`
+	Bihin       string    `json:"bihin"`
+	CreatedAt   time.Time `json:"createdat"`
 }
